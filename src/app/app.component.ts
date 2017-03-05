77b38e375fe6f0e04e5c444ac09a41f1ee2027fc
ozippy <jeremy@ozippy.com>
2017-03-05 15:26:30 +1100

    property binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { ProductsComponent } from './products/products.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+
 })
 export class AppComponent {
   title = 'app works!';
